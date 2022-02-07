fc9cfbcd40d522963baba3bb6837302310de2804
grayTurtle01 <the.correo@protonmail.com>
2022-02-07 16:33:40 -0600

    calculate the winner

--- src/index.js
+++ src/index.js
@@ -47,7 +47,17 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    let status;
+
+    let winner = calculateWinner(this.state.squares)
+
+    if ( winner ){
+      status = "The winner is " + winner
+    }
+    else{
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -101,3 +111,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
