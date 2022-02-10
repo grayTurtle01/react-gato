30ce2dc74d0a96927f4fded45ea4a3ceacd301bf
grayTurtle01 <the.correo@protonmail.com>
2022-02-10 10:42:33 -0600

    Refactor each components in a single file

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+
+function Square(props){
+
+    return (
+      <button className="square"
+              onClick={ props.handleClick }>
+
+        {props.value}
+
+      </button>
+    );
+}
+
+export default Square;
