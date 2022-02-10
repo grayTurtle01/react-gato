22b7d9a6cb83fb59f548cd6a5b1eb91eb9bbd9e0
grayTurtle01 <the.correo@protonmail.com>
2022-02-10 11:46:32 -0600

    refactor: Board component refactored from class  to function

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,43 +3,40 @@ import React from 'react'
 
 import Square from './Square'
 
-class Board extends React.Component {
+function Board(props) {
 
 
-    renderSquare(i) {
-      return <Square value={ this.props.squares[i] }
-                     handleClick={ ()=> this.props.handleClick(i) }
-                     />;
+    function renderSquare(i) {
+      return <Square value={ props.squares[i] }
+                     handleClick={ ()=> props.handleClick(i) }
+             />;
     }
-  
-    render() {
-  
-  
-      return (
+    
+    return (
         <div>
-          <div className="status">{this.props.status}</div>
+          <div className="status">{props.status}</div>
   
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
   
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
           </div>
   
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
           </div>
   
         </div>
       );
-    }
+    
 }
 
 export default Board;
